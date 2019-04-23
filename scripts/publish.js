3ef2c4a3ff0a53668f0471cfdcf20caab625ed02
Kevin Isom <kevin@isom.nz>
2019-04-23 13:17:26 +1200

    script to publish code

---
+++ scripts/publish.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const ghpages = require('gh-pages')
+
+
+module.exports = () =>
+  new Promise((resolve, reject) => {
+      return ghpages.publish(
+        path.resolve(process.cwd(), 'dist'),
+        {
+          message: 'Publish generated commit [ci skip]',
+        },
+        err => {
+          if (err) {
+            return reject(err)
+          }
+          return resolve(true)
+        }
+      )
+    })
+
