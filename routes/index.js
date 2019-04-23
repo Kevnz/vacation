000dd1680622fa58f96e18730708631c97f92434
Kevin Isom <kevin@isom.nz>
2019-04-23 13:01:31 +1200

    Big nope

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var config = require('xtconf')();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Isom Family Vacation', api: config.get('api') });
-});
-
-module.exports = router;
